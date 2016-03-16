16a89249e1acb78c2c48c7d1f967e253052786b5
strozniak <piotrek.strozniak@gmail.com>
2016-03-16 23:25:57 +0100

    tree takes hash

--- ruby/day2/tree.rb
+++ ruby/day2/tree.rb
@@ -1,12 +1,19 @@
 class Tree
-  attr_reader :name, :children
+  attr_reader :root
 
-  def initialize(name, children)
-    @name = name
-    @children = children
+  def initialize(root={})
+    @root = root
   end
 
   def visitAll(&block)
+    visit &block
+    @root.each_pair { |key, value| Tree.new(value).visitAll &block }
+  end
 
+  def visit(&block)
+    block.call @root.flatten.at(0)
   end
 end
+
+myTree = Tree.new({'stefan' => {'michael' => {'romny'=>{}}, 'roza'=>{}}})
+myTree.visitAll {|name, children| puts "#{name}" }
